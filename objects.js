aece3fa96fb54e92a4584e17cf3680d702f1c77f
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-09-17 14:28:44 -0400

    added method chaining

--- objects.js
+++ objects.js
@@ -2,19 +2,27 @@ class User {
     constructor(email, name){ //what we are expecting when the user creates a new user
         this.email = email;
         this.name = name;
+        this.score = 0; //initial score
     }
     login(){ 
         console.log(this.email, 'just logged in');
+        return this;
     }
     logout(){ 
         console.log(this.email, 'just logged out'); 
+        return this;
+    }
+    updateScore() { 
+        this.score++; 
+        console.log(this.email, 'score is now', this.score);
+        return this;
     }
 }
 
 var userOne = new User('sohil@email.com', 'sohil');
 var userTwo = new User('sparkphotos@gmail.com','spark');
-userOne.login();
-userTwo.logout();
+
+userOne.login().updateScore().updateScore().logout();
 
 // the 'new' keyword
 // creates a new empty object
