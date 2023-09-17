62d4ebc28125eb82f45d0d42ae788b6c20fdc322
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-09-17 11:50:30 -0400

    made classes & contructors

--- objects.js
+++ objects.js
@@ -1,3 +1,18 @@
 class User {
-    
+    constructor(email, name){ //what we are expecting when the user creates a new user
+        this.email = email;
+        this.name = name;
+    }
 }
+
+var userOne = new User('sohil@email.com', 'sohil');
+var userTwo = new User('sparkphotos@gmail.com','spark');
+
+console.log(userOne); 
+console.log(userTwo); 
+
+// the 'new' keyword
+// creates a new empty object
+// set the value of 'this' to be the new empty object
+// calls the contructor method
+
