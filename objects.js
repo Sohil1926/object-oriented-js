b72aef8da46a584877a9231acb5bf954b114ae91
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-09-17 15:01:37 -0400

    added admin delete function

--- objects.js
+++ objects.js
@@ -19,13 +19,25 @@ class User {
     }
 }
 
+class Admin extends User { 
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email;
+        })
+    }
+}
+
 var userOne = new User('sohil@email.com', 'sohil');
 var userTwo = new User('sparkphotos@gmail.com','spark');
-
-userOne.login().updateScore().updateScore().logout();
+var admin = new Admin('sean@email.com', 'sean');
 
 // the 'new' keyword
 // creates a new empty object
 // set the value of 'this' to be the new empty object
 // calls the contructor method
 
+var users = [userOne, userTwo, admin];
+admin.deleteUser(userOne);
+
+
+console.log(users);
